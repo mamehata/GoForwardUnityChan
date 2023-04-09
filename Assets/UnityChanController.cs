bbf3036e5f8892142e2698534e809017e0cb876c
mamehata <117148042+mamehata@users.noreply.github.com>
2023-04-10 00:38:16 +0900

    ゲームオーバーを判定
    テックアカデミーカリキュラムLesson 7Chapter 7.4

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -19,6 +19,9 @@ public class UnityChanController : MonoBehaviour
     // ジャンプの速度（追加）
     float jumpVelocity = 20;
 
+    // ゲームオーバーになる位置（追加）
+    private float deadLine = -9;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -51,6 +54,16 @@ public class UnityChanController : MonoBehaviour
             {
                 this.rigid2D.velocity *= this.dump;
             }
+        }
+
+        // デッドラインを超えた場合ゲームオーバーにする（追加）
+        if (transform.position.x < this.deadLine)
+        {
+            // UIControllerのGameOver関数を呼び出して画面上に「GameOver」と表示する（追加）
+            GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+
+            // ユニティちゃんを破棄する（追加）
+            Destroy(gameObject);
         }
     }
 }
